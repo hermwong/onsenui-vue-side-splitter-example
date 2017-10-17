feed40dfd817988ce7c5c78af3476ff81071dd52
Herm Wong <herm.wong@gmail.com>
2017-10-17 12:30:16 -0700

    moved code from LeftMenu.vue to App.vue. Deleted LeftMenu.vue since it was no longer required.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,45 @@
 <template>
-  <div id="app">
-    <left-menu />
-  </div>
+  <v-ons-splitter>
+    <v-ons-splitter-side swipeable width="150px" collapse"" side="left" :open.sync="openSide" :animation="$ons.platform.isAndroid() ? 'overlay' : 'reveal'">
+      <v-ons-page>
+        <v-ons-list>
+          <v-ons-list-item v-for="page in pages" tappable modifier="chevron" @click="currentPage = page; openSide = false">
+            <div class="center">{{ page }}</div>
+          </v-ons-list-item>
+        </v-ons-list>
+      </v-ons-page>
+    </v-ons-splitter-side>
+    <v-ons-splitter-content>
+      <component :is="currentPage" v-on:menuToggled="sideMenu"></component>
+    </v-ons-splitter-content>
+  </v-ons-splitter>
 </template>
 
 <script>
-import LeftMenu from './components/LeftMenu';
+import Home from './components/Home'
+import News from './components/News'
+import Settings from './components/Settings'
 
 export default {
   name: 'app',
   components: {
-    'left-menu': LeftMenu
+    home: Home,
+    news: News,
+    settings: Settings
+  },
+  data() {
+    return {
+      currentPage: 'home',
+      pages: ['home', 'news', 'settings'],
+      openSide: false
+    }
+  },
+  methods: {
+    sideMenu() {
+      console.log('toggleMenu handled');
+      this.openSide = !this.openSide;
+      console.log(this.openSide);
+    }
   }
 }
 </script>
